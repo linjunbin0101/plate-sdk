b9b028ac4c18514522a0bde9c1e7adc8944d952d
linjunbin0101 <2879529995@qq.com>
2026-06-29 17:54:31 +0800

    chore: bump version to 1.0.1

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -35,7 +35,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.zkc.plate"
                 artifactId = "plate-sdk"
-                version = "1.0.0"
+                version = "1.0.1"
             }
         }
     }
